a2c16a9104025c685af367288e1533d55646b706
Juha Leikas <juha.leikas@gmail.com>
2023-12-07 14:13:10 +0200

    1,2,3

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,27 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All, Or
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+    )
+)
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+   
+if __name__ == "__main__":
+    main()
